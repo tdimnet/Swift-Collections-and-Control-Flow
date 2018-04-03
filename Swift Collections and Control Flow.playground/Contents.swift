c3ae7121ab905d578aa29582995634622b6f45dd
tdimnet <thomas.dimnet@gmail.com>
2018-04-03 13:39:28 +0200

    See how to add items to arrays and how to deals with mutable/immutable arrays.

--- Swift Collections and Control Flow.playground/Contents.swift
+++ Swift Collections and Control Flow.playground/Contents.swift
@@ -6,10 +6,35 @@
  -------
  */
 
+var numbers: [Int] = [1, 2, 3]
+
 var todo: [String] = [
     "Finish collection course", // <- What it's called string literals
     "Buy groceries",
     "Respond to emails"
 ]
 
-var numbers: [Int] = [1, 2, 3]
+// Add new item to end of array using append method
+todo.append("Pick up dry cleaning")
+
+// Concatenating two arrays
+[1, 2, 3] + [4]
+numbers + [4, 5, 6]
+
+// Concatenate array containing string literal to todo
+todo + ["Order book online"] // -> only contact not update todo
+todo = todo + ["Order book online"] // -> update todo with the new itemsection course", "Buy gr
+todo += ["Continue learning Swift"] // -> alse a way to update todo array (Unary addition operator)
+
+// todo + "someString" -> will not work because of type safety
+
+
+/*
+ -------
+ Immutable Arrays
+ -------
+ */
+
+let secondTaskList: [String] = ["Mow the lawn"]
+// secondTaskList.append("Pay bills") -> error: will not work because secondTaskList is an immutable array
+// secondTaskList += ["someValue"] -> error here too
