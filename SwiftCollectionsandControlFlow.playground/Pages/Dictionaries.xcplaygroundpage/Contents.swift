79e6916dde0edbda471b2705ef3601aab664b89f
tdimnet <thomas.dimnet@gmail.com>
2018-04-03 16:27:03 +0200

    Add method for updating, adding and removing values into dictionaries.

--- SwiftCollectionsandControlFlow.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
+++ SwiftCollectionsandControlFlow.playground/Pages/Dictionaries.xcplaygroundpage/Contents.swift
@@ -13,7 +13,7 @@
         DXB                     Dubai International
 */
 
-let airportCodes: [String: String] = [
+var airportCodes: [String: String] = [
     "LGA": "La Guardia",
     "LHR": "Heathrow",
     "CDG": "Charles de Gaulle",
@@ -25,3 +25,26 @@ let personInfo: [String: Int] = [
     "ZipCode": 75000, // -> will work
     // "foo": true -> will not work because of type safety
 ]
+
+let currentWeather: [String: Double] = ["temperature": 75.0]
+
+// Reading from a dictionary
+airportCodes["LGA"] // -> airportCodes["lga"] will not work (swift is case sensitive)
+"LGA" == "lga"
+
+airportCodes["CDG"]
+
+// Inserting Key Value Pairs
+airportCodes["SYD"] = "Sydney Airport" // Will add a new entry into the dictionnary
+
+airportCodes["LGA"] = "La Guardia International Airport" // Will change the value related to the LGA key
+
+airportCodes.updateValue("Dublin Airport", forKey: "DUB") // Update a dictionnary with a new valuer
+
+airportCodes
+
+// Removing Key Value Pairs
+airportCodes["DXB"] = nil
+airportCodes.removeValue(forKey: "CDG")
+
+airportCodes
