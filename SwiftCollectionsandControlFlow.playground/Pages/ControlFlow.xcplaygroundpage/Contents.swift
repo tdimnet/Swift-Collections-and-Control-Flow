4f90481fbe5d985ca5b54165874751708f32dc0b
tdimnet <thomas.dimnet@gmail.com>
2018-04-05 13:28:59 +0200

    Add while and repeat while loop

--- SwiftCollectionsandControlFlow.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
+++ SwiftCollectionsandControlFlow.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -20,3 +20,42 @@ for task in todo {
 for number in 1...10 {
     print("\(number) times 5 is equal to \(number * 5)")
 }
+
+
+// While Loop
+var x: Int = 0
+
+while x <= 20 {
+    print(x)
+    x += 1
+}
+
+var index: Int = 0
+
+while index < todo.count {
+    print(todo[index])
+    index += 1
+}
+
+
+// Repeat While
+
+var counter: Int = 1
+
+while counter < 1 {
+    print("I am inside the while loop")
+    counter += 1
+}
+
+repeat {
+    print("I am inside the repeat loop")
+    counter += 1
+} while counter < 1
+
+
+
+
+
+
+
+
