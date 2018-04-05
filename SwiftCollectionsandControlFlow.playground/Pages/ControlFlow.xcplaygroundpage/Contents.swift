074cb44da5e8cf28ddd3cb1306f32e6522580378
tdimnet <thomas.dimnet@gmail.com>
2018-04-05 13:05:16 +0200

    Start working with for loop in Swift

---
+++ SwiftCollectionsandControlFlow.playground/Pages/ControlFlow.xcplaygroundpage/Contents.swift
@@ -0,0 +1,16 @@
+let todo: [String] = [
+    "Finish collections course",
+    "Buy groceries",
+    "Respond to emails",
+    "Pick up dry cleaning",
+    "Order books online",
+    "Mow the lawn"
+]
+
+//let firstItem: String = todo[0]
+//print(firstItem)
+//print(todo[0])
+
+for task in todo {
+    print(task)
+}
