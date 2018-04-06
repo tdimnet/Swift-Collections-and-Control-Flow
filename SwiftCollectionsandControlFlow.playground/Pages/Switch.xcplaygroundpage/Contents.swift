c145f3267166b8ee4acb6e27754ea97c6e071eb9
tdimnet <thomas.dimnet@gmail.com>
2018-04-06 10:42:41 +0200

    Add the final challenge of the course and update switch operators.

--- SwiftCollectionsandControlFlow.playground/Pages/Switch.xcplaygroundpage/Contents.swift
+++ SwiftCollectionsandControlFlow.playground/Pages/Switch.xcplaygroundpage/Contents.swift
@@ -1,14 +1,67 @@
 // Switch Statement
 
-let airportCodes: [String] = ["LGA", "LHR", "CDG", "HKG", "DXB"]
+let airportCodes: [String] = ["LGA", "LHR", "CDG", "HKG", "DXB", "LGW", "JFK", "ORY"]
 
 for airportCode in airportCodes {
     switch airportCode {
-    case "LGA": print("New York")
-    case "LHR": print("London")
-    case "CDG": print("Paris")
+    case "LGA", "JFK": print("New York")
+    case "LHR", "LGW": print("London")
+    case "CDG", "ORY": print("Paris")
     // case 1: print("Int") <- Will not work because of type safety
     case "HKG": print("Hong Kong")
     default: print("I don't know which city that airport is in!")
     }
 }
+
+
+import GameKit
+
+let randomTemperature = GKRandomSource.sharedRandom().nextInt(upperBound: 150)
+
+switch randomTemperature {
+case 0..<32:
+    print("Forget clothes, you are basically a popsicle")
+case 32..<45:
+    print("It's quite cold. You will need a jacket")
+case 45..<70:
+    print("It's a bit chilly. I recommend wearing a light sweater")
+case 70...100:
+    print("It's quite hot! T-shirt weather!")
+default:
+    print("Sorry, I am not alive anymore.")
+}
+
+
+var europeanCapitals: [String] = []
+var asianCapitals: [String] = []
+var otherCapitals: [String] = []
+
+let world = [
+    "BEL": "Brussels",
+    "LIE": "Vaduz",
+    "BGR": "Sofia",
+    "USA": "Washington D.C.",
+    "MEX": "Mexico City",
+    "BRA": "Brasilia",
+    "IND": "New Delhi",
+    "VNM": "Hanoi"]
+
+for (key, value) in world {
+    // Enter your code below
+    switch key {
+    case "BEL", "LIE", "BGR":
+        europeanCapitals.append(value)
+    case "IND", "VNM":
+        asianCapitals.append(value)
+    default:
+        otherCapitals.append(value)
+    }
+    // End code
+}
+
+
+
+
+
+
+
